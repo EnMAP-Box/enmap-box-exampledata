32b109d0b29dbc63961a3a5c49dac921bd9d5926
Andreas Janz <andreas.janz@geo.hu-berlin.de>
2023-10-06 11:04:10 +0200

    deleted all data files; updated readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # enmap-box-exampledata
+
+This repository did not contain the actual data. Data is stored here:
+
+Potsdam dataset (current): https://github.com/EnMAP-Box/enmap-box/tree/main/enmapbox/exampledata
+
+Berlin dataset (old): https://github.com/EnMAP-Box/enmap-box/tree/main/tests/testdata/exampledata/berlin
+
+This repository is only used for releasing the datasets, e.g.
+https://github.com/EnMAP-Box/enmap-box-exampledata/releases/tag/v1.0
--- enmapboxexampledata/berlin/__init__.py
+++
@@ -1,9 +0,0 @@
-from os.path import dirname, join
-
-enmap_berlin = join(dirname(__file__), 'enmap_berlin.bsq')
-enmap_srf_library = join(dirname(__file__), 'enmap_srf_library.gpkg')
-hires_berlin = join(dirname(__file__), 'hires_berlin.bsq')
-landcover_berlin_point = join(dirname(__file__), 'landcover_berlin_point.gpkg')
-landcover_berlin_polygon = join(dirname(__file__), 'landcover_berlin_polygon.gpkg')
-library_berlin = join(dirname(__file__), 'library_berlin.gpkg')
-veg_cover_fraction_berlin_point = join(dirname(__file__), 'veg-cover-fraction_berlin_point.gpkg')
